2b64b747fbb58f76352c5cd6af70e3e83da15828
Akhat <chopin3224@gmail.com>
2024-03-16 14:00:59 +0500

    sol

--- python.py
+++ python.py
@@ -1,13 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        for index in range (len(nums)):
-            j = index + 1
-            if (index + 1 < len(nums)):
-                if nums[index] == nums[j]:
-                    nums.pop(j)
+        if len(nums) == 1:
+            return 1
+        i = 0
+        j = i + 1
+        while True:
+            if (j >=  len(nums)):
+                break
+            if (nums[i] == nums[j]):
+                nums.pop(j)
+                continue
+            i += 1
+            j += 1
+        print(nums)
         return len(nums)
 
-nums = [0,0,1,1,1,2,2,3,3,4]
+nums = [1,1,2]
 sol = Solution()
 asd = sol.removeDuplicates(nums)
 print(asd)

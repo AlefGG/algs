351a92326b9eab13c4641e0bdb5f48ef479e5662
Akhat <chopin3224@gmail.com>
2024-03-18 10:08:22 +0500

    s

--- python.py
+++ python.py
@@ -1,13 +1,31 @@
-class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
-        for index in range (len(nums)):
-            j = index + 1
-            if (index + 1 < len(nums)):
-                if nums[index] == nums[j]:
-                    nums.pop(j)
-        return len(nums)
+class MinStack:
 
-nums = [0,0,1,1,1,2,2,3,3,4]
-sol = Solution()
-asd = sol.removeDuplicates(nums)
-print(asd)
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

5611ba9e30a25d1aa85715ee4e546291393ab456
Akhat <chopin3224@gmail.com>
2024-03-19 22:27:21 +0500

    1

--- python.py
+++ python.py
@@ -3,4 +3,23 @@ class ListNode:
         self.val = val
         self.next = next
 
+class LinkedList:
+    def __init__(self):
+        self.head = ListNode(-1)
+        self.tail = self.head
 
+    def toString(self, list: ListNode):
+        res = []
+        while list:
+            res.append(list.val)
+            list = list.next
+        print(res)
+
+    def createLinkedListFromArray(self, lst: list) -> ListNode:
+        head = ListNode() 
+        curr = head
+
+        for val in lst:
+            curr.next = ListNode(val)  
+            curr = curr.next  
+        return head.next 

23096dab011c416706ab6a411b1be99835fb3481
Akhat <chopin3224@gmail.com>
2024-03-19 13:35:08 +0500

    1

--- python.py
+++ python.py
@@ -4,3 +4,14 @@ class ListNode:
         self.next = next
 
 
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        curr = head 
+        while curr.next:
+            dummy = curr.next
+            curr.next = curr.next.next
+            dummy.next = head
+            head = dummy
+        return head
